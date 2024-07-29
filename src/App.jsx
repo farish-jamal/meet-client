decb6f116b83f87fc7bf866f1973deb048312433
farish-jamal <farishjamal98@gmail.com>
2024-07-29 21:17:16 +0530

    Feat: Protected Routed added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Protected from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Register />} />
+      <Route path="/" element={<Protected element={<Home />} />} />
     </Routes>
   );
 };
