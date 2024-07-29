b4252df4f4914e4ae57535d0c8634237d1d80c61
farish-jamal <farishjamal98@gmail.com>
2024-07-29 21:05:54 +0530

    Routed added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Register from "./pages/Register"
+import { Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <>
-     <Register />
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Register />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
