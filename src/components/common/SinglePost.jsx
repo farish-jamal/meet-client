5cde3fe51826760f1af91a4e112b58e1993fec95
farish-jamal <farishjamal98@gmail.com>
2024-08-05 17:30:49 +0530

    Added Single Post

--- src/components/common/SinglePost.jsx
+++ src/components/common/SinglePost.jsx
@@ -1,128 +1,184 @@
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { Heart, MessageCircle, Share } from 'lucide-react';
-import { useState } from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Heart, MessageCircle, Share } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const SinglePost = () => {
- const [comments, setComments] = useState([
-  { userName: 'johndoe', text: 'Amazing post! Love the view!' },
-  { userName: 'janedoe', text: 'Wow, this place looks beautiful.' },
-  { userName: 'user123', text: 'Can you share the location of this place?' },
-]);
-const [newComment, setNewComment] = useState('');
+  const [post, setPost] = useState([]);
+  const [comments, setComments] = useState([
+    { userName: "johndoe", text: "Amazing post! Love the view!" },
+    { userName: "janedoe", text: "Wow, this place looks beautiful." },
+    { userName: "user123", text: "Can you share the location of this place?" },
+  ]);
+  const [newComment, setNewComment] = useState("");
 
-const handleCommentChange = (e) => {
-  setNewComment(e.target.value);
-};
+  const { id } = useParams();
 
-const handleCommentSubmit = () => {
-  if (newComment.trim()) {
-    setComments([...comments, { userName: 'currentuser', text: newComment }]);
-    setNewComment('');
-  }
-};
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
 
-const post = {
-  user: {
-    profile: "https://res.cloudinary.com/dacwig3xk/image/upload/v1722170520/onnangimuklpf0m1vknc.jpg",
-    userName: "samaninggg",
-  },
-  image: "https://res.cloudinary.com/dacwig3xk/image/upload/v1722759038/qda6ovezc7gcmfs4dxbs.jpg",
-  caption: "Enjoying the beautiful view! #Travel #Nature",
-  likes: 120,
-  comments: 35,
-};
+  const handleCommentSubmit = () => {
+    if (newComment.trim()) {
+      setComments([...comments, { userName: "currentuser", text: newComment }]);
+      setNewComment("");
+    }
+  };
+
+  const handleGetPost = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/api/user/post/${id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (!response.ok) {
+        toast.error("Error while fetching data");
+        return;
+      }
+      const postData = await response.json();
+      setPost(postData.data);
+      console.log(postData.data);
+    } catch (error) {
+      toast.error("Internal server error");
+      console.log(error);
+    }
+  };
+
+  useState(() => {
+    handleGetPost();
+  }, []);
+
   return (
-   <div className="flex-1 lg:mx-4 lg:my-4 bg-white py-4 px-4 mb-20 md:mb-20 lg:px-5 rounded-lg shadow-lg overflow-y-auto no-scrollbar">
-   <div className="flex items-center p-4 border-b border-gray-300">
-        <Avatar className="w-14 h-14 rounded-full overflow-hidden">
-          <AvatarImage
-            src={post.user.profile}
-            alt={post.user.userName}
-            className="object-cover w-full h-full"
-          />
-          <AvatarFallback className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-700">
-            {post.user.userName[0].toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
-        <div className="ml-4">
-          <p className="text-xl font-semibold text-gray-800">{post.user.userName}</p>
-        </div>
-      </div>
-
-      {/* Post Image */}
-      <div className="relative">
-        <img
-          src={post.image}
-          alt="Post"
-          className="w-full h-auto object-cover rounded-t-xl border-b border-gray-200"
-        />
-      </div>
-
-      {/* Post Actions */}
-      <div className="flex items-center p-4 border-b border-gray-300">
-        <div className="flex items-center space-x-4">
-          <button className="p-2 rounded-full hover:bg-gray-100 transition">
-            <Heart className="w-7 h-7 text-red-500" />
-          </button>
-          <button className="p-2 rounded-full hover:bg-gray-100 transition">
-            <MessageCircle className="w-7 h-7 text-gray-700" />
-          </button>
-          <button className="p-2 rounded-full hover:bg-gray-100 transition">
-            <Share className="w-7 h-7 text-gray-700" />
-          </button>
-        </div>
-      </div>
-
-      {/* Post Details */}
-      <div className="p-4 ">
-        <p className="font-semibold text-lg text-gray-800">{post.likes} likes</p>
-        <p className="text-gray-700 mt-2">{post.caption}</p>
-        <p className="text-gray-500 mt-2">{post.comments} comments</p>
-      </div>
-
-      {/* Comment Section */}
-      <div className="p-4">
-        {/* Comment Input */}
-        <div className="flex items-center overflow-hidden border-b border-gray-300 pb-6">
-          <Avatar className="w-10 h-10 rounded-full overflow-hidden">
-            <AvatarFallback className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-700">
-              U
-            </AvatarFallback>
-          </Avatar>
-          <input
-            type="text"
-            value={newComment}
-            onChange={handleCommentChange}
-            className="flex-1 ml-2 px-4 py-2 border-none rounded-lg outline-none placeholder-gray-500"
-            placeholder="Add a comment..."
-          />
-          <button
-            onClick={handleCommentSubmit}
-            className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-          >
-            Post
-          </button>
-        </div>
-
-        {/* Comment List */}
-        <div className="space-y-4 pt-6">
-          {comments.map((comment, index) => (
-            <div key={index} className="flex items-start gap-3">
+    <>
+      {post.caption ? (
+        <div className="flex-1 lg:mx-4 lg:my-4 bg-white py-4 px-4 mb-20 md:mb-20 lg:px-5 rounded-lg shadow-lg overflow-y-auto no-scrollbar">
+          <div className="flex items-center p-4 border-b border-gray-300">
+            <Avatar className="w-14 h-14 rounded-full overflow-hidden">
+              <AvatarImage
+                src={post.image}
+                alt={post._id}
+                className="object-cover w-full h-full"
+              />
+              {post.user && (
+                <AvatarFallback className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-700">
+                  {post.user.userName[0].toUpperCase()}
+                </AvatarFallback>
+              )}
+            </Avatar>
+            {post.user && (
+              <div className="ml-4">
+                <p className="text-xl font-semibold text-gray-800">
+                  {post.user.userName}
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="relative">
+            <img
+              src={post.image}
+              alt="Post"
+              className="w-full h-[500px] object-cover rounded-t-xl border-b border-gray-200"
+            />
+          </div>
+          <div className="flex items-center p-4 border-b border-gray-300">
+            <div className="flex items-center space-x-4">
+              <button className="p-2 rounded-full hover:bg-gray-100 transition">
+                <Heart className="w-7 h-7 text-red-500" />
+              </button>
+              <button className="p-2 rounded-full hover:bg-gray-100 transition">
+                <MessageCircle className="w-7 h-7 text-gray-700" />
+              </button>
+              <button className="p-2 rounded-full hover:bg-gray-100 transition">
+                <Share className="w-7 h-7 text-gray-700" />
+              </button>
+            </div>
+          </div>
+          <div className="p-4 ">
+            <p className="font-semibold text-lg text-gray-800">12 likes</p>
+            <p className="text-gray-700 mt-2">{post.caption}</p>
+            <p className="text-gray-500 mt-2">10 comments</p>
+          </div>
+          <div className="p-4">
+            <div className="flex items-center overflow-hidden border-b border-gray-300 pb-6">
               <Avatar className="w-10 h-10 rounded-full overflow-hidden">
                 <AvatarFallback className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-700">
-                  {comment.userName[0].toUpperCase()}
+                  U
                 </AvatarFallback>
               </Avatar>
-              <div>
-                <p className="font-semibold text-gray-800">{comment.userName}</p>
-                <p className="text-gray-700">{comment.text}</p>
-              </div>
+              <input
+                type="text"
+                value={newComment}
+                onChange={handleCommentChange}
+                className="flex-1 ml-2 px-4 py-2 border-none rounded-lg outline-none placeholder-gray-500"
+                placeholder="Add a comment..."
+              />
+              <button
+                onClick={handleCommentSubmit}
+                className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+              >
+                Post
+              </button>
             </div>
-          ))}
+            <div className="space-y-4 pt-6">
+              {comments.map((comment, index) => (
+                <div key={index} className="flex items-start gap-3">
+                  <Avatar className="w-10 h-10 rounded-full overflow-hidden">
+                    <AvatarFallback className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-700">
+                      {comment.userName[0].toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="font-semibold text-gray-800">
+                      {comment.userName}
+                    </p>
+                    <p className="text-gray-700">{comment.text}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
- </div>
-  )
-}
+      ) : (
+        <div className="flex-1 lg:mx-4 lg:my-4 bg-white py-4 px-4 mb-20 md:mb-20 lg:px-5 rounded-lg shadow-lg overflow-y-auto no-scrollbar">
+          <div className="flex items-center p-4 border-b border-gray-300">
+            <Skeleton className="h-16 w-16 rounded-full" />
+            <div className="ml-4">
+              <Skeleton className="h-10 w-32 lg:w-[96]" />
+            </div>
+          </div>
+          <div className="relative">
+            <Skeleton className="h-[500px]" />
+          </div>
+          <div className="flex items-center p-4 border-b border-gray-300">
+            <div className="flex items-center space-x-4">
+              <Skeleton className="h-8 w-8" />
+              <Skeleton className="h-8 w-8" />
+              <Skeleton className="h-8 w-8" />
+            </div>
+          </div>
+          <div className="p-4 space-y-4">
+            <Skeleton className="h-6 w-72" />
+            <Skeleton className="h-6 w-72" />
+            <Skeleton className="h-6 w-72" />
+          </div>
+          <div className="p-4">
+            <div className="flex items-center overflow-hidden pb-6">
+              <Skeleton className="h-10 w-10 rounded-full" />
+              <Skeleton className="h-10 w-[90%] rounded-lg ml-2" />
+              <Skeleton className="ml-2 h-10 w-[10%]" />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default SinglePost
+export default SinglePost;
